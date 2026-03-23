a3deacfa068c0f2b4fe44584d613ac182981ddd6
alvescunhasd-hub <alvescunha.sd@gmail.com>
2026-03-23 15:51:26 +0000

    Create index.js

---
+++ public/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../assistant_vitrage';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
